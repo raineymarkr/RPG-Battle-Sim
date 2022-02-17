9be1e5e7296584486dc6815d605ba905987288a8
raineymarkr <97651651+raineymarkr@users.noreply.github.com>
2022-02-17 14:53:07 -0600

    Add files via upload

---
+++ src/Combat.h
@@ -0,0 +1,19 @@
+#ifndef COMBAT_H
+#define COMBAT_H
+
+#include "Character.h"
+#include "Monster.h"
+
+class Combat
+{
+    public:
+        Combat(Monster& C);
+
+        Monster& M;
+
+        void combatMenu(Character &C);
+        void combat1(Character &C);
+
+};
+
+#endif // COMBAT_H
---
+++ src/DiceRoller.h
@@ -0,0 +1,13 @@
+#ifndef DICEROLLER_H
+#define DICEROLLER_H
+
+
+class DiceRoller
+{
+    public:
+        int rollDie();
+
+
+};
+
+#endif // DICEROLLER_H
